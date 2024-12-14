ba856ac263846f2a29887b2c7756e5a4f8e6938c
Amina <paru@kemono.se>
2024-12-15 00:22:40 +0200

    Enable logo.gen.h again

--- platform/osx/export/export.cpp
+++ platform/osx/export/export.cpp
@@ -44,7 +44,7 @@
 #include "editor/editor_node.h"
 #include "editor/editor_settings.h"
 #include "modules/modules_enabled.gen.h" // For regex.
-//#include "platform/osx/logo.gen.h"
+#include "platform/osx/logo.gen.h"
 
 #include <sys/stat.h>
 
